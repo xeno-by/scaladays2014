7b76f8c1e09f3e3cea856a90e3ed400b75e0a1b9
Eugene Burmako <xeno.by@gmail.com>
2014-06-17 03:55:09 +0200

    sets the stage in intellij

--- intellij/src/Test.scala
+++ intellij/src/Test.scala
@@ -1,19 +1,5 @@
-import scala.language.experimental.macros
-import scala.meta._
-import scala.meta.semantic._
-import errors.throwExceptions
-
 object Test extends App {
-  def join[T, U](x: T, y: U): Any = macro {
-    val xfields = x.tpe.vals.map(f => f -> q"xtemp")
-    val yfields = y.tpe.vals.map(f => f -> q"ytemp")
-    val getters = (xfields ++ yfields).map{ case (f, ref) => q"val ${f.name} = $ref.${f.name}" }
-    c.whitebox(q"""
-      val xtemp = $x
-      val ytemp = $y
-      new { ..$getters }
-    """)
-  }
-  val result = join(new { val x = 2 }, new { val y = 2 })
+  def join[T, U](x: T, y: U): Any = macro ???
+  val result = join(new { val x = 2 }, new { val y = 3 })
   println((result.x, result.y))
 }
